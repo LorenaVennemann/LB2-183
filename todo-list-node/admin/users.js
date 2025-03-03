a41258e1dd71cf978274ea2a1f7a2e7c9dc94f9f
Sandro <sandro.kohler@edu.tbz.ch>
2025-03-03 11:32:06 +0100

    passwörter aus der view entfernt und verunmöglicht xss

--- todo-list-node/admin/users.js
+++ todo-list-node/admin/users.js
@@ -3,7 +3,7 @@ const db = require('../fw/db');
 async function getHtml() {
     let conn = await db.connectDB();
     let html = '';
-    let [result,fields] = await conn.query("SELECT users.ID, users.username, users.password, roles.title FROM users inner join permissions on users.ID = permissions.userID inner join roles on permissions.roleID = roles.ID order by username");
+    let [result, fields] = await conn.query("SELECT users.ID, users.username, roles.title FROM users INNER JOIN permissions ON users.ID = permissions.userID INNER JOIN roles ON permissions.roleID = roles.ID ORDER BY username");
 
     html += `
     <h2>User List</h2>
@@ -16,13 +16,17 @@ async function getHtml() {
         </tr>`;
 
     result.map(function (record) {
-        html += `<tr><td>`+record.ID+`</td><td>`+record.username+`</td><td>`+record.title+`</td><input type='hidden' name='password' value='`+record.password+`' /></tr>`;
+        html += `<tr><td>${sanitize(record.ID)}</td><td>${sanitize(record.username)}</td><td>${sanitize(record.title)}</td></tr>`;
     });
 
     html += `
     </table>`;
 
     return html;
+}
+
+function sanitize(input) {
+    return input.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
 }
 
 module.exports = { html: getHtml() };
