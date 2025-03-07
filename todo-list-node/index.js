eaa7e47dcca9eb33fdf3f897de2c7d6c74a480fe
Sandro <sandro.kohler@edu.tbz.ch>
2025-03-07 18:46:41 +0100

    secure index.js

--- todo-list-node/index.js
+++ todo-list-node/index.js
@@ -1,9 +1,11 @@
 const tasklist = require('./user/tasklist');
 const bgSearch = require('./user/backgroundsearch');
+const escapeHtml = require('escape-html');
 
 async function getHtml(req) {
     let taskListHtml = await tasklist.html(req);
-    return `<h2>Welcome, `+req.cookies.username+`!</h2>` + taskListHtml + '<hr />' + bgSearch.html(req);
+    let username = escapeHtml(req.cookies.username);
+    return `<h2>Welcome, ` + username + `!</h2>` + taskListHtml + '<hr />' + bgSearch.html(req);
 }
 
 module.exports = {
