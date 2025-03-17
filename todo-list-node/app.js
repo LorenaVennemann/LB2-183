1642e7ca039d57c810f0fcf55df71f8ea930690c
linustimodaniels <linustimo.daniels@gmail.com>
2025-03-17 12:59:30 +0100

    google login implemented

--- todo-list-node/app.js
+++ todo-list-node/app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const cors = require('cors');
 const header = require('./fw/header');
 const footer = require('./fw/footer');
-const login = require('./login');
 const index = require('./index');
 const adminUser = require('./admin/users');
 const editTask = require('./edit');
@@ -14,8 +14,8 @@ const saveTask = require('./savetask');
 const search = require('./search');
 const searchProvider = require('./search/v2/index');
 const register = require('./register');
+const login = require('./login');
 const auth = require('./auth');
-const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
@@ -42,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.use('/auth', register);
+app.use('/login', login);
 
 // Middleware zur Überprüfung der Authentifizierung
 app.use(async (req, res, next) => {
@@ -61,59 +62,19 @@ app.use(async (req, res, next) => {
 // Routen
 app.get('/', async (req, res) => {
     if (req.user) {
-        let html = await wrapContent(await index.html(req), req)
+        let html = await wrapContent(await index.html(req), req);
         res.send(html);
     } else {
-        res.redirect('login');
-    }
-});
-
-app.post('/login', async (req, res) => {
-    let content = await login.handleLogin(req, res);
-    let html = await wrapContent(content.html, req);
-    res.redirect('/');
-});
-
-// edit task
-app.get('/admin/users', async (req, res) => {
-    if (req.user) {
-        let html = await wrapContent(await adminUser.html, req);
-        res.send(html);
-    } else {
-        res.redirect('/');
-    }
-});
-
-// edit task
-app.get('/edit', async (req, res) => {
-    if (req.user) {
-        let html = await wrapContent(await editTask.html(req), req);
-        res.send(html);
-    } else {
-        res.redirect('/');
-    }
-});
-
-// Login-Seite anzeigen
-app.get('/login', async (req, res) => {
-    let content = await login.handleLogin(req, res);
-
-    if (content.user.userid !== 0) {
-        // login was successful... set cookies and redirect to /
-        login.startUserSession(res, content.user);
-    } else {
-        // login unsuccessful or not made jet... display login form
-        let html = await wrapContent(content.html, req);
-        res.send(html);
+        res.redirect('/login');
     }
 });
 
 // Logout
 app.get('/logout', (req, res) => {
     req.session.destroy();
-    res.cookie('username', '');
-    res.cookie('userid', '');
-    res.cookie('idToken', '');
+    res.clearCookie('username');
+    res.clearCookie('userid');
+    res.clearCookie('idToken');
     res.redirect('/login');
 });
 
@@ -127,7 +88,25 @@ app.get('/profile', async (req, res) => {
     }
 });
 
-// save task
+// Andere Routen bleiben unverändert
+app.get('/admin/users', async (req, res) => {
+    if (req.user) {
+        let html = await wrapContent(await adminUser.html, req);
+        res.send(html);
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/edit', async (req, res) => {
+    if (req.user) {
+        let html = await wrapContent(await editTask.html(req), req);
+        res.send(html);
+    } else {
+        res.redirect('/');
+    }
+});
+
 app.post('/savetask', async (req, res) => {
     if (req.user) {
         let html = await wrapContent(await saveTask.html(req), req);
@@ -137,13 +116,11 @@ app.post('/savetask', async (req, res) => {
     }
 });
 
-// search
 app.post('/search', async (req, res) => {
     let html = await search.html(req);
     res.send(html);
 });
 
-// search provider
 app.get('/search/v2/', async (req, res) => {
     let result = await searchProvider.search(req);
     res.send(result);
