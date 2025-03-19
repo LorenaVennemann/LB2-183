eca6ec87a033f9af92bca736449588e9cc2fe2c9
Sandro <sandro.kohler@edu.tbz.ch>
2025-03-19 18:29:22 +0100

    moved css

--- todo-list-node/fw/header.js
+++ todo-list-node/fw/header.js
@@ -17,7 +17,7 @@ async function getHtml(req) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TBZ 'Secure' App</title>
-    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="/css/styles.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
 </head>
